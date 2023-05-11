bb9a4831a3215804b93f7b17df959e25664d34cb
Sergey Molodkin <molodkin.98@mail.ru>
2023-05-11 10:22:25 +0300

    working on orderService

--- internal/customer/routes/ordersRoutes/createOrder.go
+++ internal/customer/routes/ordersRoutes/createOrder.go
@@ -1,1 +1,10 @@
 package ordersRoutes
+
+type CreateOrderRequestBody struct {
+	UserUUID string `json:"user_uuid"`
+}
+
+type CustomerOrderItem struct {
+	Count       int    `json:"count"`
+	ProductUUID string `json:"product_uuid"`
+}
